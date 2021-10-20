032c5da486d05fc3ccd22cae5c294c708708eff3
lidachao111222 <chao.li@usask.ca>
2021-10-20 02:01:53 -0600

    uptade the ipfs images, display element

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { useEtherBalance, useEthers } from '@usedapp/core';
 import { formatEther } from '@ethersproject/units';
 import ContractInteraction from './components/contractInteraction';
-import {pinata_api_key,pinata_secret_api_key} from './utils'
+import { pinata_api_key, pinata_secret_api_key } from './utils'
 import './App.css';
 
 let ringElement;
@@ -11,7 +11,11 @@ let ringElement;
 function App() {
 
   const [str, setStr] = useState(" ");
-  const { activateBrowserWallet, account, deactivate,library } = useEthers();
+
+  const [imgdir, setImgdir] = useState("");
+
+
+  const { activateBrowserWallet, account, deactivate, library } = useEthers();
   const etherBalance = useEtherBalance(account);
   const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
 
@@ -26,16 +30,17 @@ function App() {
       maxBodyLength: 'Infinity',
       headers: { 'Content-Type': `multipart/form-data; boundary=${data._boundary}`, pinata_api_key: pinata_api_key, pinata_secret_api_key: pinata_secret_api_key }
     }).then((val) => {
-      console.log(val)
+      console.log(val?.data.IpfsHash)
       console.log("success")
-      alert("success")
+      alert(`success,your hash is ${val?.data.IpfsHash}`)
+      setImgdir(`https://gateway.pinata.cloud/ipfs/${val?.data.IpfsHash}`)
     })
   }
 
   return (
     <div className="App">
       {console.log(library)}
-      <ContractInteraction/>
+      <ContractInteraction />
       {!account && <button onClick={activateBrowserWallet}> Connect </button>}
       {account && <button onClick={deactivate}> Disconnect </button>}
 
@@ -44,6 +49,12 @@ function App() {
       {etherBalance && <p>Balance: {formatEther(etherBalance)}</p>}
 
 
+      <div style={{ width: "400px", margin: "0 auto" }}>
+        
+        {imgdir === "" ? <></> :<> success,your image link is  here: <a href={imgdir}>{imgdir}</a><img src={imgdir} alt="nft" /></>}
+      </div>
+
+      your demo:
       <div style={{ width: "400px", margin: "0 auto" }}>
         <nft-ring ref={(el) => ringElement = el} r-min-ratio=".2" r-max-ratio="1.5" colors="#007F10,#1fE45e,#AEB32E,#6649A6,#FE1777,#fB8F3F,#CB29E3,#4ABBDF,#9839EA,#533874"></nft-ring>
       </div>
