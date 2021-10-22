1ee63b36067ac5ecd40aaf1906f543e84e3e26e7
stevewu <42365700+Stevewu1234@users.noreply.github.com>
2021-10-22 16:25:58 +0800

    add sign function

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
-export const pinata_api_key = "";
+export const pinata_api_key = "ab23fd8fb7e5273acf7a";
 
-export const pinata_secret_api_key = ""
+export const pinata_secret_api_key = "81c60113cfbb844b3fa1bcf698cdf592bcaed828bdcda3c4a774295bed47f739"
 
 export const  chainIdUrl = "https://kovan.infura.io/v3/"
